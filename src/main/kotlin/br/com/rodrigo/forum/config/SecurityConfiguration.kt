89d5d2eac425a5a52468176b2d12bf349e1e087e
Rodrigo de Oliveira <rodrigodotnet@outlook.com>
2022-04-13 16:51:20 -0300

    feat: autenticação jwt

--- src/main/kotlin/br/com/rodrigo/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/rodrigo/forum/config/SecurityConfiguration.kt
@@ -1,8 +1,11 @@
 package br.com.rodrigo.forum.config
 
+import br.com.rodrigo.forum.security.JwtAuthenticationFilter
+import br.com.rodrigo.forum.security.JwtLoginFilter
 import br.com.rodrigo.forum.services.UsuarioService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -10,23 +13,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration(private val usuarioService: UsuarioService) : WebSecurityConfigurerAdapter() {
+class SecurityConfiguration(
+    private val usuarioService: UsuarioService,
+    private val jwtUtil: JwtUtil
+) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
-        http?.authorizeRequests()
-            ?.antMatchers("/topicos")
-            ?.hasAnyAuthority("LEITURA_ESCRITA")
+        val rotas: Array<String> = arrayOf("/topicos", "/cursos", "/usuarios", "/respostas")
+
+        http?.csrf()?.disable()
+            ?.authorizeRequests()
+            ?.antMatchers(*rotas)?.hasAnyAuthority("LEITURA_ESCRITA")
+            ?.antMatchers(HttpMethod.POST, "/login")?.permitAll()
             ?.anyRequest()
             ?.authenticated()
             ?.and()
-            ?.sessionManagement()
+        http?.addFilterBefore(
+            JwtLoginFilter(authManager = authenticationManager(), jwtUtil = jwtUtil),
+            UsernamePasswordAuthenticationFilter().javaClass
+        )
+        http?.addFilterBefore(JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http?.sessionManagement()
             ?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            ?.and()?.formLogin()
-            ?.disable()
-            ?.httpBasic()
     }
 
     override fun configure(auth: AuthenticationManagerBuilder?) {
